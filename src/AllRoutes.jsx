b85329070c9d42530798f2bacd29c4bb5614b2a3
neongutsyyy <amitmohanty403@gmail.com>
2023-07-29 00:25:40 +0530

    added login and signup

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import{Route,Routes} from 'react-router-dom';
 import Homepage from './Pages/Homepage';
+import LoginPage from './Components/Login/Login';
+import RegisterPage from './Components/Signup/Signup';
 const AllRoutes= () => 
 {
     return(
         <Routes>
             <Route path='/' element={<Homepage/>}></Route>
+            <Route path='/login' element={<LoginPage/>}></Route>
+            <Route path='/signup' element={<RegisterPage/>}></Route>
         </Routes>
     )
 }
--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import AllRoutes from './AllRoutes';
 import Navbar1 from './Components/Navbar1';
+
 function App() {
   return (
     <div>
-      <Navbar1 />
+      <Navbar1 /> 
       <AllRoutes />
     </div>
-    
   );
 }
 export default App;
